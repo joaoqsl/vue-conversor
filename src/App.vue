282bde067f31db504feb4a53664c285cdcc08a70
JoaoPauloETEC <joaojpmoreira25@gmail.com>
2019-11-18 02:19:47 -0300

    Interface e funcionalidades

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="text-center">Conversor de Moedas</h1>
+    <div class="linha">
+      <Conversor moeda="Dólar americano para real" moedaA="USD" moedaB="BRL"/>
+      <Conversor moeda="Real para dólar americano" moedaA="BRL" moedaB="USD"/>
+    </div>
+    <div class="linha">
+      <Conversor moeda="Dólar canadense para real" moedaA="CAD" moedaB="BRL"/>
+      <Conversor moeda="Real para dólar canadense" moedaA="BRL" moedaB="CAD"/>
+    </div>
+    <div class="linha">
+      <Conversor moeda="Euro para real" moedaA="EUR" moedaB="BRL"/>
+      <Conversor moeda="Real para euro" moedaA="BRL" moedaB="EUR"/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Conversor from './components/Conversor.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Conversor
   }
 }
 </script>
 
 <style>
 #app {
+  margin: auto;
+  max-width: 900px;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.linha{
+  margin-top: 40px;
+  display: flex;
+  justify-content: space-around;
+}
+.text-center{
+  text-align: center;
 }
 </style>
